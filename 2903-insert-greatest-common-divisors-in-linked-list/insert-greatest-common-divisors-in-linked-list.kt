89727c064b8afa95678ed25be0ecd52b5335ffaf
AhmedElshaer <ahmed3elshaer@gmail.com>
2024-09-10 15:43:55 +0300

    Time: 259 ms (26.67%) | Memory: 39.3 MB (20.00%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.kt
@@ -0,0 +1,43 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun insertGreatestCommonDivisors(head: ListNode?): ListNode? {
+        var current = head
+        var next = current?.next
+        while(current?.next != null){
+            next = current?.next // 6
+            val d = greatestCommonDivisor(current?.`val`!!, next?.`val`!!) // 
+            val newNode = ListNode(d) // 6 -> 6
+            newNode.next = next
+            current?.next = newNode // 18 -> 6 -> 6
+            current = newNode.next
+        }
+        return head
+    }
+
+    fun greatestCommonDivisor(first : Int, second: Int) :Int{
+        if(first == second) return first
+        var top = first
+        var bottom = first
+         if(first > second){
+            top = first; bottom = second;
+        }else{
+            top = second; bottom = first;
+        }
+        if(top % bottom == 0){
+            return bottom
+        }
+        var res = bottom
+        while(top % res != 0 || bottom % res != 0){
+            res--
+        }
+        return res
+    }
+}
